5804ad59c07f5533b48711c6bbd0d870ae8b386b
Alex McKenzie <here.is.alex@gmail.com>
2021-11-10 01:29:53 -0600

    just starting out in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header(props) {
+  console.log(props);
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around!</p>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li>{dish}</li>
+          ))}
+      </ul>
+    </section>
+
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Bean Curd Dish",
+  "Chicken Breast",
+  "Tofu with Vegetables",
+  "Egg Pizza",
+  "Oatmeal with Tuna"
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Xiaowei" />
+      <Main adjective="amazing" dishes={dishes}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
